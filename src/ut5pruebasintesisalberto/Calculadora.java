9e1b6824e11e7bcef9d07acdeacd4a1637c35e04
Alberto Viera <Alberto Viera>
2016-04-08 16:55:01 +0100

    paso 1 y 2

---
+++ src/ut5pruebasintesisalberto/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5pruebasintesisalberto;
+
+/**
+ *
+ * @author Albbe
+ */
+public class Calculadora {
+    private int operador1;
+    private int operador2;
+    private int operacion;
+    
+    
+}
